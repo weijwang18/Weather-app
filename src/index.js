651be6c480b64e99f77d7cab6ac635d9d690b625
Winnie Wang <weijwang18@gmail.com>
2022-05-20 03:20:12 -0700

    Added weather icon and changed the layout

--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", handleSubmit);
 
 function displayWeather(response) {
+  tempC = response.data.main.temp;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
   document.querySelector("#cityName").innerHTML = response.data.name;
-  document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#temp").innerHTML = Math.round(tempC);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -60,5 +65,30 @@ today.innerHTML = `${day}  `;
 
 let time = document.querySelector("#time");
 time.innerHTML = `${hour}:${minutes}`;
+
+function showFahrenheintTemperature(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+  celsiuslink.classList.remove("active");
+  fahrenheintLink.classList.add("active");
+  let tempF = (tempC * 9) / 5 + 32;
+  temp.innerHTML = Math.round(tempF);
+}
+
+function showCelsiuslinkTemperature(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+  celsiuslink.classList.add("active");
+  fahrenheintLink.classList.remove("active");
+  temp.innerHTML = Math.round(tempC);
+}
+
+let tempC = null;
+
+let fahrenheintLink = document.querySelector("#fahrenheit-link");
+fahrenheintLink.addEventListener("click", showFahrenheintTemperature);
+
+let celsiuslink = document.querySelector("#celsius-link");
+celsiuslink.addEventListener("click", showCelsiuslinkTemperature);
 
 search("Seattle");
