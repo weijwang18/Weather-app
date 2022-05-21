013caeea41aae649eace66fd13c7426c691df139
Winnie Wang <weijwang18@gmail.com>
2022-05-21 05:31:27 -0700

    Display the forecast data using API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let apiKey = "2da3b808285a97f89d4570afa4ceaaf7";
+var apiKey = "2da3b808285a97f89d4570afa4ceaaf7";
 
 function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -22,6 +22,48 @@ function handleSubmit(event) {
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", handleSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML += `
+     <div class="col-2">
+       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+       
+       <img src="http://openweathermap.org/img/wn/${
+         forecastDay.weather[0].icon
+       }@2x.png" alt="" width="42" />
+       <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max"> ${Math.round(
+           forecastDay.temp.max
+         )}° </span>
+         <span class="weather-forecast-temperature-min"> ${Math.round(
+           forecastDay.temp.min
+         )}° </span>
+       </div>
+     </div>
+   `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
   tempC = response.data.main.temp;
   document
@@ -38,6 +80,7 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  getForecast(response.data.coord);
 }
 
 function getPostion(postion) {
@@ -107,3 +150,4 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", showCelsiuslinkTemperature);
 
 search("Seattle");
+displayForecast();
